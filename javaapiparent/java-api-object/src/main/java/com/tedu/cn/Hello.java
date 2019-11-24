0568fe6fa07e278c108c502aa2aa307fbc9fcac1
Huowenjing2019 <378099027@qq.com>
2019-11-24 16:30:36 +0800

    第8次提交

--- javaapiparent/java-api-object/src/main/java/com/tedu/cn/Hello.java
+++ javaapiparent/java-api-object/src/main/java/com/tedu/cn/Hello.java
@@ -3,7 +3,9 @@ package com.tedu.cn;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("你睡觉的");
-        System.out.println("zdjf");
-    }
+        System.out.println("你睡觉的");
+        System.out.println("你睡觉的");
+        System.out.println("你睡觉的");
 
+    }
 }
